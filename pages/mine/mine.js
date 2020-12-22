cc1f39a3abfcd9d6ddbeaeab2830595d3294f165
“于永鑫 <yuyongxin@situdata.com>
2020-12-22 17:40:24 +0800

    no message

--- pages/mine/mine.js
+++ pages/mine/mine.js
@@ -6,9 +6,58 @@ Page({
    */
   data: {
     userIcon: '/images/xcxsy/u144.jpg',
-    mySeeList: ["/images/xcxsy/u144.jpg", "/images/xcxsy/u144.jpg"]
+    mySeeList: ["/images/xcxsy/u144.jpg", "/images/xcxsy/u144.jpg"],
+    sheetList: [
+      { text: '清除', value: 1 },
+    ],
+    sheetLoginOutList: [
+      { text: '退出登录', value: 1 },
+    ],
+    showActionsheet: false,
+    showLoginOutSheet: false
+  },
+  // 打开登出抽屉
+  openSheetLogin() {
+    wx.hideTabBar({
+      animation: false,
+    })
+    this.setData({showLoginOutSheet : true})
+  },
+  // 打开清除抽屉
+  openSheet() {
+    console.log('show')
+    wx.hideTabBar({
+      animation: false,
+    })
+    this.setData({showActionsheet : true})
+  },
+  // 关闭抽屉
+  closeSheet() {
+    wx.showTabBar({
+      animation: false,
+    })
   },
 
+   // 点击抽屉
+  btnClick(e) {
+    console.log(e, 'e')
+  },
+   // 点击登出抽屉
+   btnLoginOutClick(e) {
+    console.log(e, 'e')
+  },
+  // 修改资料
+  jumptoeditUserInfo() {
+    let self = this
+    console.log(1, self.userIcon, this.data.userIcon)
+    wx.navigateTo({
+      url: '../editUser/editUser',
+      success: function(res) {
+        // 通过eventChannel向被打开页面传送数据
+        res.eventChannel.emit('acceptDataFromOpenerPage', { id: '110',userIcon: self.data.userIcon })
+      }
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
@@ -27,7 +76,10 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-
+    
+    wx.showTabBar({
+      animation: false,
+    })
   },
 
   /**
