3647ba06f74394dd3ec23bcace3f734be27c67e5
yuyx <2293709008>
2021-01-07 19:35:32 +0800

    直播中

---
+++ palette/card1.js
@@ -0,0 +1,155 @@
+
+    export default class LastMayday {
+      palette(obj) {
+        return (
+{
+  "width": "255px",
+  "height": "350px",
+  "background": "#f8f8f8",
+  "views": [
+    {
+      "type": "image",
+      "url": obj.img,
+      "css": {
+        "width": "194.1908713692946px",
+        "height": "245px",
+        "top": "23px",
+        "left": "31px",
+        "rotate": "0",
+        "borderRadius": "",
+        "borderWidth": "",
+        "borderColor": "#000000",
+        "shadow": "",
+        "mode": "scaleToFill"
+      }
+    },
+    {
+      "type": "image",
+      "url": "/images/saveshare.png",
+      "css": {
+        "width": "20.912863070539466px",
+        "height": "13.46153846153849px",
+        "top": "0.5400000000000018px",
+        "left": "156.6997510373444px",
+        "rotate": "0",
+        "borderRadius": "",
+        "borderWidth": "",
+        "borderColor": "#000000",
+        "shadow": "",
+        "mode": "scaleToFill"
+      }
+    },
+    {
+      "type": "qrcode",
+      "content": "哈哈哈",
+      "css": {
+        "color": "#000000",
+        "background": "#ffffff",
+        "width": "56.80082987551867px",
+        "height": "58.5px",
+        "top": "281.5px",
+        "left": "169.3609958506224px",
+        "rotate": "0",
+        "borderRadius": ""
+      }
+    },
+    {
+      "type": "image",
+      "url": "/images/saveshare.png",
+      "css": {
+        "width": "28.680497925311204px",
+        "height": "18.461538461538463px",
+        "top": "307.54px",
+        "left": "35.33045643153526px",
+        "rotate": "0",
+        "borderRadius": "",
+        "borderWidth": "",
+        "borderColor": "#000000",
+        "shadow": "",
+        "mode": "scaleToFill"
+      }
+    },
+    {
+      "type": "text",
+      "text": "大学长",
+      "css": {
+        "color": "#000000",
+        "background": "rgba(0,0,0,0)",
+        "width": "50px",
+        "height": "21.449999999999996px",
+        "top": "-3.552713678800501e-15px",
+        "left": "180.52697095435684px",
+        "rotate": "0",
+        "borderRadius": "",
+        "borderWidth": "",
+        "borderColor": "#000000",
+        "shadow": "",
+        "padding": "0px",
+        "fontSize": "15px",
+        "fontWeight": "normal",
+        "maxLines": "2",
+        "lineHeight": "21.645000000000003px",
+        "textStyle": "fill",
+        "textDecoration": "none",
+        "fontFamily": "",
+        "textAlign": "left"
+      }
+    },
+    {
+      "type": "text",
+      "text": "吃喝玩乐",
+      "css": {
+        "color": "#4D86F1",
+        "background": "rgba(0,0,0,0)",
+        "width": "100px",
+        "height": "21.449999999999996px",
+        "top": "319px",
+        "left": "66.93px",
+        "rotate": "0",
+        "borderRadius": "",
+        "borderWidth": "",
+        "borderColor": "#000000",
+        "shadow": "",
+        "padding": "0px",
+        "fontSize": "15px",
+        "fontWeight": "normal",
+        "maxLines": "2",
+        "lineHeight": "21.645000000000003px",
+        "textStyle": "fill",
+        "textDecoration": "none",
+        "fontFamily": "",
+        "textAlign": "left"
+      }
+    },
+    {
+      "type": "text",
+      "text": "小仙女",
+      "css": {
+        "color": "#000000",
+        "background": "rgba(0,0,0,0)",
+        "width": "100px",
+        "height": "21.449999999999996px",
+        "top": "302px",
+        "left": "70.81px",
+        "rotate": "0",
+        "borderRadius": "",
+        "borderWidth": "",
+        "borderColor": "#000000",
+        "shadow": "",
+        "padding": "0px",
+        "fontSize": "15px",
+        "fontWeight": "normal",
+        "maxLines": "2",
+        "lineHeight": "21.645000000000003px",
+        "textStyle": "fill",
+        "textDecoration": "none",
+        "fontFamily": "",
+        "textAlign": "left"
+      }
+    }
+  ]
+}
+        );
+      }
+    }
+    
